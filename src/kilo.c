d009ede80803772395d2d3980e93aea9a6ae821a
Carles Amigó <fr3nd@fr3nd.net>
2018-01-13 13:13:38 +0100

    including msx calls and improving program

--- src/kilo.c
+++ src/kilo.c
@@ -1,17 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "asm.h"
 
-void enableRawMode() {
+Z80_registers regs;
+
+#define _TERM0  0x00
+#define _TERM   0x62
+#define _DOSVER 0x6F
+#define _INITXT 0x006C
+
+#define perror(x) {printf("*** ");printf(x);printf("\r\n");}
+
+/*** functions ***/
+
+void die(const char *s) {
+  perror(s);
+  regs.Bytes.B = 1;
+  DosCall(_TERM, &regs, REGS_NONE, REGS_NONE);
 }
 
-void disableRawMode() {
+void init() {
+  static char __at(0xF3AE) LINL40;
+
+  // Check MSX-DOS version >= 2
+  DosCall(_DOSVER, &regs, REGS_NONE, REGS_MAIN);
+  if(regs.Bytes.B < 2) {
+    die("This program requires MSX-DOS 2 to run.");
+  }
+
+  // Set 80 column mode
+  LINL40 = 80;
+  BiosCall(_INITXT, &regs, REGS_NONE);
 }
+
+void quit_program() {
+}
+
+/*** main ***/
 
 int main() {
   char c;
 
-  enableRawMode();
+  init();
 
   do {
     c = getchar();
@@ -22,6 +53,6 @@ int main() {
     }
   } while (c != 'q');
 
-  disableRawMode();
+  quit_program();
   return 0;
 }
